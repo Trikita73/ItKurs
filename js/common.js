4a211781ac17cfd00a71d58aa12de36be380a8b2
andrii <diachenkoandrii7@gmail.com>
2017-02-11 18:39:01 +0200

    start_animate

--- js/common.js
+++ js/common.js
@@ -30,6 +30,11 @@ $(document).ready(function() {
 		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 
+	$(".bottom_phone .wrapper .tab").click(function() {
+		$(".bottom_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
 	$(".tabs_header .wrapper .tab").click(function() {
 		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()

738e8c9e720f47c5455d69ec50d56fa544b923f5
andrii <diachenkoandrii7@gmail.com>
2018-01-04 19:30:14 +0200

    title

--- mail.php
+++ mail.php
@@ -7,5 +7,5 @@ $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone";
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient"); 
